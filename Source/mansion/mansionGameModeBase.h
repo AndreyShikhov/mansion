d02e03753b91005dac6a722fff7331f2790d9278
test <Andrey.Shikhov94@yandex.ru>
2021-01-27 22:24:06 +0500

    the fiirst commit

---
+++ Source/mansion/mansionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "mansionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MANSION_API AmansionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
